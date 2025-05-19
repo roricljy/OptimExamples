36aafa955102e265800bb2729353fdc199037512
roricljy <roricljy@gmail.com>
2025-05-19 14:51:21 +0900

    update

---
+++ root_finding.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# Settings
+learning_rate = 0.01
+tolerance = 1e-12
+max_iter = 1000
+
+# Model prediction
+def model(x):
+    return x**3 + x**2 + x + 1
+
+# Gradient Descent Search
+x = 0
+for iter in range(max_iter):
+    residual = model(x) - 0
+    f = residual**2
+    fp = 2*residual*(3*x**2 + 2*x + 1)
+    x_new = x - learning_rate * fp
+    print(f"iter{iter}: loss = {f}")
+    if abs(x_new - x) < tolerance:
+        break
+    x = x_new
+
+# Print the result
+print(f"The root is {x}")
+print(f"model({x}) = {model(x)}")
