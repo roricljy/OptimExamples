977f6f45f690adb7469d79676580ed576dba8ff0
roricljy <roricljy@gmail.com>
2025-06-09 20:55:06 +0900

    update

--- parabolafit_weighted_ls.py
+++ parabolafit_weighted_ls.py
@@ -7,7 +7,7 @@ plt.rcParams.update({'font.size': 14*gscale})
 # Generate data
 x = np.arange(1, 101)
 y = -2 * (x - 40)**2 + 30 + 100 * np.random.randn(100)
-y[49:70] += 500 * np.abs(np.arange(50, 71) - 60) - 5000
+y[50:80] += 400 * np.abs(np.arange(50, 80) - 65) - 7000
 
 # Create matrix A
 A = np.vstack([x**2, x, np.ones(len(x))]).T
@@ -20,7 +20,7 @@ plt.figure(figsize=(7*gscale, 7*gscale))
 plt.pause(0.1)
 
 # Iterative algorithm
-for itr in range(15):
+for itr in range(100):
     r = A @ p - y
     W = np.diag(1 / (1 + np.abs(r) / 1.3998))
     p = np.linalg.inv(A.T @ W @ A) @ (A.T @ W @ y)
@@ -30,7 +30,7 @@ for itr in range(15):
     plt.text(0.35, 0.5, f"Iteration {itr}", transform=plt.gca().transAxes, fontsize=20, color="black", bbox=bkg)
     plt.plot(x, y, '*b', markersize=6*gscale)
     plt.plot(x, A @ p, 'r', linewidth=3*gscale)
-    plt.pause(0.5)
+    plt.pause(0.2)
     plt.clf()
 
 # Plot the final result
